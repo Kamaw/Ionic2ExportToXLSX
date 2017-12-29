1ca2e6069f09a689f4b2a08abb184e01d3baa52e
chintansoni54 <ghanu@9426067993>
2017-12-29 12:27:38 +0530

    Final Code

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,44 +1,89 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {File} from '@ionic-native/file';
+import * as XLSX from 'xlsx';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
 
-  public data : [object];
-  constructor() {
-    
-      this.data = [{
-        Tower : "A",
-        Flat  : "101",
-        Month : "November",
-        Year  : "2017"
-      }, {
-        Tower : "A",
-        Flat  : "201",
-        Month : "November",
-        Year  : "2017"
-      }, {
-        Tower : "B",
-        Flat  : "301",
-        Month : "November",
-        Year  : "2017"
-      }, {
-        Tower : "C",
-        Flat  : "101",
-        Month : "November",
-        Year  : "2017"
-      }, {
-        Tower : "D",
-        Flat  : "401",
-        Month : "November",
-        Year  : "2017"
-      }]
-  }
+    data: [object];
 
-  OnExport = function(){
-      alert("hello");
-  }
-  
+    constructor(public file: File)
+    {
+        this.data = [{
+            Tower: "A",
+            Flat: "101",
+            Month: "November",
+            Year: "2017"
+        }, {
+            Tower: "A",
+            Flat: "201",
+            Month: "November",
+            Year: "2017"
+        }, {
+            Tower: "B",
+            Flat: "301",
+            Month: "November",
+            Year: "2017"
+        }, {
+            Tower: "C",
+            Flat: "101",
+            Month: "November",
+            Year: "2017"
+        }, {
+            Tower: "D",
+            Flat: "401",
+            Month: "November",
+            Year: "2017"
+        }];
+    }
+
+    getStoragePath()
+    {
+        let file = this.file;
+        return this.file.resolveDirectoryUrl(this.file.externalRootDirectory).then(function (directoryEntry) {
+            return file.getDirectory(directoryEntry, "Ionic2ExportToXLSX", {
+                create: true,
+                exclusive: false
+            }).then(function () {
+                return directoryEntry.nativeURL + "Ionic2ExportToXLSX/";
+            });
+        });
+    }
+
+    OnExport = function ()
+    {
+        let sheet = XLSX.utils.json_to_sheet(this.data);
+        let wb = {
+            SheetNames: ["export"],
+            Sheets: {
+                "export": sheet
+            }
+        };
+
+        let wbout = XLSX.write(wb, {
+            bookType: 'xlsx',
+            bookSST: false,
+            type: 'binary'
+        });
+
+        function s2ab(s) {
+            let buf = new ArrayBuffer(s.length);
+            let view = new Uint8Array(buf);
+            for (let i = 0; i != s.length; ++i) view[i] = s.charCodeAt(i) & 0xFF;
+            return buf;
+        }
+
+        let blob = new Blob([s2ab(wbout)], {type: 'application/octet-stream'});
+        let self = this;
+        this.getStoragePath().then(function (url) {
+            self.file.writeFile(url, "export.xlsx", blob, true).then(() => {
+                alert("file created at: " + url);
+            }).catch(() => {
+                alert("error creating file at :" + url);
+            });
+        });
+    }
 }
